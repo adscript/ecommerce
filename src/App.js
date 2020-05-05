9ca2f0ebe7fe285ff3fbc0f5b01bc0636127c218
Adnan Radja Maulana <adnan-macbook@Adnans-MacBook-Pro.local>
2020-05-05 15:14:45 +0700

    update menuitem with routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage.component'
+import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './menu-item.styles.scss'
-function MenuItem({ title, imageUrl, size }) {
+import { withRouter } from 'react-router-dom'
+function MenuItem({ title, imageUrl, size, linkUrl, history, match }) {
     return (
-        <div className={`${size} menu-item`} >
-            <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}/>
+        <div
+            className={`${size} menu-item`}
+            onClick={() => history.push(`${match.url}${linkUrl}`)}
+        >
+            <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">SHOP NOW</span>
@@ -12,4 +16,4 @@ function MenuItem({ title, imageUrl, size }) {
     )
 }
 
-export default MenuItem
+export default withRouter(MenuItem)

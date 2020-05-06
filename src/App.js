58bff8d458424d0392f63737a2699885249aedd9
Adnan Radja Maulana <adnan-macbook@Adnans-MacBook-Pro.local>
2020-05-06 14:01:14 +0700

    add navigation header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={Shop} />

c9f3110fee01ed39657b8a5ce9f025ebacf2d48f
Adnan Radja Maulana <adnan-macbook@Adnans-MacBook-Pro.local>
2020-05-16 13:36:02 +0700

    cart hidden redux implemented

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.styles.scss'
 import { auth } from '../../firebase/firebase.utils'
 import { connect } from 'react-redux'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
-function Header({ currentUser }) {
+function Header({ currentUser, hidden }) {
     return (
         <div className="header">
             <Link to='/' className="logo-container">
@@ -25,12 +27,15 @@ function Header({ currentUser }) {
                         SIGN IN
                 </Link>
                 }
+                <CartIcon />
             </div>
+            {hidden ? null : <CartDropdown />}
         </div>
     )
 }
 const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+    currentUser: state.user.currentUser,
+    hidden: state.cart.hidden
 })
 
 export default connect(mapStateToProps)(Header)

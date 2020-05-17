565140f0c4a5822cb2b5c75fa275d5ebaeb6ebd1
Adnan Radja Maulana <adnan-macbook@Adnans-MacBook-Pro.local>
2020-05-17 15:17:06 +0700

    handling multiple cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import CustomButton from '../custom-button/custom-button.component'
+import { addItem } from '../../redux/cart/cart.action'
+
 import './collection-item.styles.scss'
 
-function CollectionItem({name, price, imageUrl}) {
+function CollectionItem({
+    item,
+    addItem
+}) {
+    const { name, price, imageUrl } = item
     return (
         <div className="collection-item">
             <div
@@ -12,8 +21,14 @@ function CollectionItem({name, price, imageUrl}) {
                 <div className='name'>{name}</div>
                 <div className='price'>{`$${price}`}</div>
             </div>
+            <CustomButton inverted onClick={() => addItem(item)}>Add to Cart</CustomButton>
         </div>
     )
 }
 
-export default CollectionItem
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+
+export default connect(null, mapDispatchToProps)(CollectionItem)

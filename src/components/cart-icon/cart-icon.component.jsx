0633bbebb30eb4b7284af1525fd7941d0b2cad5b
Adnan Radja Maulana <adnan-macbook@Adnans-MacBook-Pro.local>
2020-05-17 16:25:24 +0700

    modifying cart with optimizing memoization using selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,18 +4,23 @@ import { connect } from 'react-redux'
 import { toggleCartHidden } from '../../redux/cart/cart.action'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
+import {selectCartItemsCount } from '../../redux/cart/cart.selectors'
 import './cart-icon.styles.scss'
-function CartIcon({ toggleCartHidden }) {
+function CartIcon({ toggleCartHidden, totalCount }) {
     return (
         <div className="cart-icon" onClick={toggleCartHidden}>
             <ShoppingIcon className="shopping-icon" />
-            <span className="item-count">0</span>
+            <span className="item-count">{totalCount}</span>
         </div>
     )
 }
+
+const mapStateToProps = state => ({
+    totalCount: selectCartItemsCount(state)
+})
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)

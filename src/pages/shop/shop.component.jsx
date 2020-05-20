f8cb1b142ddd160f568b4ec1666eb35d82f43fb7
Adnan Radja Maulana <adnan-macbook@Adnans-MacBook-Pro.local>
2020-05-20 09:15:48 +0700

    moved static data to reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,14 @@
-import React, { Component } from 'react'
-import SHOP_DATA from './shop.data'
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import React from 'react'
+import { Route } from 'react-router-dom'
 
-export default class Shop extends Component {
-    constructor () {
-        super()
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({ id, ...otherCollectionsProps }) => (
-                        <CollectionPreview key={id} {...otherCollectionsProps}/>
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const Shop = ({ match }) => (
+    <div className='shop-page' >
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+    </div >
+)
+
+export default Shop
